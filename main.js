2b503217a36eae887bcf9dd2605c379653023940
ketigabunia15-hash <ketigabunia15@gmail.com>
2025-11-21 18:00:43 +0400

    CountryByCurrency

--- main.js
+++ main.js
@@ -7,3 +7,18 @@ console.log(isEmpty("abc")); // false
 function convertToGEL(priceUSD, rate) {
     return priceUSD * rate;
 }
+
+function getCountryByCurrency(code) {
+    if (code === "USD") {
+        return "United States";
+    } 
+    else if (code === "EUR") {
+        return "European Union";
+    } 
+    else if (code === "GEL") {
+        return "Georgia";
+    } 
+    else {
+        return "Unknown currency";
+    }
+}
